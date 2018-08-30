864f7f41cb55a878933dc283120a1c3d1b995fd9
isan <38833845+isanbitoy@users.noreply.github.com>
2018-08-30 23:24:27 +0800

    Add files via upload

--- src/components/PortfolioPanel.vue
+++ src/components/PortfolioPanel.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
-
     <div class="filter-buttons">
-      <button v-on:click="filter('all')">all</button>
-      <button v-on:click="filter('tag1')">tag 1</button>
+      <a class="topnav-item" v-on:click="shuffle"><span class="item">SHUFFLE</span></a>
+      <a class="topnav-item" v-on:click="filter('all')"><span class="item">ALL</span></a>
+      <a class="topnav-item" v-on:click="filter('tag1')"><span class="item">TAG 1</span></a>
     </div>
 
-	  <div class="grid-container">
-      <div class="portfolio-content" v-for="content in filteredContents" v-bind:key="id">
+    <transition-group name="grid-content" class="grid-content" tag="section">
+      <div class="item-content" v-for="content in filteredContents" v-bind:key="content.id">
         <img class="img"v-bind:src="require('../assets/images/' + content.img)" />
         <h3 class="title">{{ content.title }}</h3>
         <p class="desc">{{ content.desc }}</p>
       </div> 
-    </div>
-
+    </transition-group>
   </div>
 </template>
 
 <script>
+import _ from 'lodash'
 export default {
 	data() {
 		return {
@@ -86,23 +86,65 @@ export default {
   methods: {
     filter: function(tag) {
       this.currentTag = tag;
+    },
+    shuffle: function() {
+      this.contents = _.shuffle(this.contents)
     }
   }
 }
 </script>
 
 <style>
-.grid-container { 
+.filter-buttons {
+    margin-bottom: 15px;
+}
+.topnav-item {
+  display: inline-flex;
+  color: #A6A6A6;
+  text-align: center;
+  justify-content: center;
+  padding: 18px 20px;
+  text-decoration: none;
+  font-size: 24px;
+}
+.topnav-item:hover {
+  color: #333;
+}
+.item {
+  position: relative;
+  cursor: pointer;
+}
+.item::after {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  transform: scaleX(0);
+  transform-origin: bottom;
+  transition: transform 0.25s ease-out;
+}
+.item:hover::after {
+  background-color: #333;
+  transform: scaleX(1);
+  transform-origin: bottom center;
+}
+.grid-content { 
     display: grid;
     grid-gap: 6px;
     grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
     grid-auto-rows: 1fr;
     grid-auto-flow: dense;
+    transition: all;
 }
-.portfolio-content {
+.item-content {
 	  background: #d3d3d3;
 }
-.filter-buttons {
-    margin-bottom: 15px;
+.grid-content-enter-active, .grid-content-leave-active {
+    transition: opacity 0.5s ease-out;
+}
+.grid-content-enter, .grid-content-leave-to {
+    opacity: 0;
 }
 </style>
